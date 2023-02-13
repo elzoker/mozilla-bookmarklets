f287ff40dc57ba447544b6ee92299cd9f426f947
elzoker <125075498+elzoker@users.noreply.github.com>
2023-02-13 20:04:07 +0100

    Update copy-taburls-to-clipboard.js

--- copy-taburls-to-clipboard.js
+++ copy-taburls-to-clipboard.js
@@ -1,11 +1,15 @@
 javascript:(function() {
-    let urls = [];
-    let tabs = window.gBrowser.browsers;
-    for (let i = 0; i < tabs.length; i++) {
-        urls.push(tabs[i].currentURI.spec);
+    try {
+        let urls = [];
+        let tabs = window.gBrowser.browsers;
+        for (let i = 0; i < tabs.length; i++) {
+            urls.push(tabs[i].currentURI.spec);
+        }
+        urls = urls.join("\n");
+        Components.classes["@mozilla.org/widget/clipboardhelper;1"]
+          .getService(Components.interfaces.nsIClipboardHelper)
+          .copyString(urls);
+    } catch (e) {
+        alert("An error occurred while trying to copy the URLs: " + e);
     }
-    urls = urls.join("\n");
-    Components.classes["@mozilla.org/widget/clipboardhelper;1"]
-      .getService(Components.interfaces.nsIClipboardHelper)
-      .copyString(urls);
 })();
